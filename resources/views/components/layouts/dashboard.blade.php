c11290de17935ac8c4a808444da45707c19fb03e
Gilberto Carrillo <gilbertocp93@gmail.com>
2023-05-08 07:18:44 -0400

    Add layouts.side-bar view and layouts.dashboard view

---
+++ resources/views/components/layouts/dashboard.blade.php
@@ -0,0 +1,10 @@
+<div class="container-fluid overflow-hidden">
+    <div class="row vh-100 overflow-auto">
+        <div class="d-none d-md-flex col-md-3 col-xl-2 flex-column flex-shrink-0 p-3 text-white bg-dark">
+            <x-layouts.side-bar />
+        </div>
+        <div class="col h-100">
+            {{ $slot }}
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,19 @@
 <x-layouts.app title="Home">
-    <div class="vh-100">
-        {{ Auth::user() }}
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <a href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                                this.closest('form').submit();">
-                Logout
-            </a>
-    </div>
+    <x-layouts.dashboard>
+        @if (!Auth::user()->pricelist)
+            <div class="h-100 d-flex flex-column align-items-center justify-content-center">
+                <div class="fs-4 text-secondary mb-2">
+                    Price list not found
+                </div>
+
+                <a href="{{ route('home') }}" class="btn btn-primary">
+                    Create a Price list
+                </a>
+            </div>
+        @else
+            <div class="display-4">
+                Home
+            </div>
+        @endif
+    </x-layouts.dashboard>
 </x-layouts.app>
