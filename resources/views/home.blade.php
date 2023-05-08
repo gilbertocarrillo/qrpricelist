423018e49ac9ceafd6fe0028f310834bdf8eb62c
Gilberto Carrillo <gilbertocp93@gmail.com>
2023-05-08 02:33:59 -0400

    Add layouts.nav component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,3 @@
 <x-layouts.app title="Home">
-    <div class="lead">
-        Home
-    </div>
-
-    <div>
-        {{ Auth::user() }}
-    </div>
-
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <a href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                        this.closest('form').submit();">
-            Logout
-        </a>
-    </form>
-
+    <x-layouts.nav />
 </x-layouts.app>
