460a852f6c898d957d03a4923c38d0ab20a2ed2a
Gilberto Carrillo <gilbertocp93@gmail.com>
2023-05-07 20:24:18 -0400

    Add layouts.app component

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+@props(['title'])
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
+    <title>{{ $title }}</title>
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite(['resources/css/app.scss', 'resources/js/app.js'])
-</head>
-<body>
+<x-layouts.app title="Home">
     <div class="lead">
         Home
     </div>
-</body>
-</html>
+</x-layouts.app>
+
